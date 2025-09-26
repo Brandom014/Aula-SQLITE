a200e47cfb19b2308086f8cfb4c81eb5845f22e5
Brandom <brandom.quispe2020@gmail.com>
2025-09-26 13:53:28 -0300

    Função deletar aluno

--- main.py
+++ main.py
@@ -7,15 +7,15 @@ conexao = sqlite3.connect("escola.db")
 #Criar o objeto chamado de "cursor" que será usado para executar os comandos sql
 cursor = conexao.cursor()
 
-#Criar uma tabela no banco
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS alunos (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    nome TEXT NOT NULL,
-    idade INTEGER,
-    curso TEXT               
-    )
-""")
+# #Criar uma tabela no banco
+# cursor.execute("""
+# CREATE TABLE IF NOT EXISTS alunos (
+#     id INTEGER PRIMARY KEY AUTOINCREMENT,
+#     nome TEXT NOT NULL,
+#     idade INTEGER,
+#     curso TEXT               
+#     )
+# """)
 #nome = input("Digite o nome do aluno: ").lower()
 #idade = int(input("Digite a idade do aluno: "))
 #curso = input("Digite o curso do aluno: ").lower()
@@ -73,3 +73,9 @@ CREATE TABLE IF NOT EXISTS alunos (
 # print(f"Alunos do curso de {pesquisar}")
 # for linha in cursor.fetchall():
 #     print(f"NOME: {linha[0]} | IDADE: {linha[1]}")
+
+#Deletar dados do banco.
+cursor.execute("DELETE FROM alunos WHERE id = ?", (1,))
+conexao.commit()
+#Sempre fechar a conexão com o banco de dados
+conexao.close()
